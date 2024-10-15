cb3363f90dcccad3572057e78a4dcb87f4d7708a
Your Name <you@example.com>
2024-10-15 08:48:25 +0530

    Add 15/10

--- src/CrudBtn.jsx
+++ src/CrudBtn.jsx
@@ -1,13 +1,14 @@
 import { Field, Form, Formik } from 'formik'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import { addData, deleteData, updateData } from "./FeildCrud";
-
+import './App.css';
 
 const CrudBtn = () => {
-  const dataItem = useSelector((state) => state.feilds.data)
-  const initialval = useSelector((state) => state.feilds.initialval)
-  // console.log("data=>", dataItem);
+  const dataItem = useSelector((state) => state.feilds.data);
+  console.log(dataItem);
+  
+  const initialval = useSelector((state) => state.feilds.initialval);
   const dispatch = useDispatch();
   const [editIndex, setEditIndex] = useState(null);
 
@@ -16,14 +17,14 @@ const CrudBtn = () => {
   }
 
   return (
-    <div>
+    <div className="container">
       <h1>Redux CRUD</h1>
       <Formik
         enableReinitialize
         initialValues={editIndex !== null ? dataItem[editIndex] : initialval}
-
-        onSubmit={(values,{resetForm}) => {
+        onSubmit={(values, { resetForm }) => {
           // console.log(values);
+          
           if (editIndex === null) {
             dispatch(addData(values));
           } else {
@@ -31,65 +32,53 @@ const CrudBtn = () => {
             setEditIndex(null); // Reset editIndex after updating
           }
           resetForm();
-
         }}
       >
         <Form>
+          <label>Name</label>
+          <Field type="text" name="name" className="form-control" placeholder="Enter name" />
+          
+          <label>Email</label>
+          <Field type="email" name="email" className="form-control" placeholder="Enter email" />
+          
+          <label>Password</label>
+          <Field type="password" name="password" className="form-control" placeholder="Enter password" />
 
-          Name : <Field
-            type="text"
-            name="name"
-          ></Field><br/>
-
-          Email : <Field
-            type="email"
-            name="email"
-          ></Field><br/>
-
-          Password : <Field
-            type="password"
-            name="password"
-          ></Field><br/><br/>
-
-          <button type='submit'>SUBMIT</button>
-
-
+          <button type='submit' className="btn btn-primary mt-3">
+            {editIndex === null ? 'Add Data' : 'Update Data'}
+          </button>
         </Form>
       </Formik>
 
-
-      <br />
-
-      <table border={1}>
-        <tr>
-          <th>Name</th>
-          <th>Email ID</th>
-          <th>Password</th>
-          <th>Action</th>
-        </tr>
-
-        {
-          dataItem.map((item, i) => (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email ID</th>
+            <th>Password</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataItem.map((item, i) => (
             <tr key={i}>
               <td>{item.name}</td>
               <td>{item.email}</td>
               <td>{item.password}</td>
               <td>
-                <button type='button' onClick={() => dispatch(deleteData(i))}>DELETE</button>&nbsp;
-                <button type='button' onClick={() => editData(i)}>EDIT</button>
+                <button type='button' className="btn btn-delete" onClick={() => dispatch(deleteData(i))}>
+                  Delete
+                </button>
+                <button type='button' className="btn btn-edit" onClick={() => editData(i)}>
+                  Edit
+                </button>
               </td>
             </tr>
-          ))
-
-        }
-
+          ))}
+        </tbody>
       </table>
-
-
     </div>
   )
 }
 
-export default CrudBtn
-
-
+export default CrudBtn;
